de4d66dde53369d9fd587c212e4fac9638113254
ahsaankhatri <ahsaanrafiq8@gmail.com>
2023-08-10 00:36:24 +0500

    Added the API for Fetching the data

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -3,83 +3,62 @@ import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import './Search.css'
 
-const Search = () => {
-  return (
-    <div>
-      <Header />
-      <div id="page">
-        <div class="container">
-            <section class="search-result-sec">
-                <div class="flex-row">
-                    <div class="col-2">
-                     <div class="banner-ad">
-                        <p>Banner Ad</p>
+const Search = (props) => {
+    console.log("Fetched Data :", props.data);
+
+        return (
+            <div>
+              {/* <Header /> */}
+              <div id="page">
+                <div className="container">
+                    <section className="search-result-sec">
+                        <div className="flex-row">
+                            <div className="col-2">
+                             <div className="banner-ad">
+                                <p>Banner Ad</p>
+                            </div>
+                        </div>
+                        <div className="col-8">
+                            <div className="search-results">
+                                <p>Your Results</p>
+                                {props.data && props.data.length > 0 ? (
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Product Name</th>
+                                            <th>Price</th>
+                                            <th>PPU</th>
+                                            <th>Buy Now</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    {props.data.map((itemData, index) => (
+                                    <tr key={index}>
+                                        <td>{itemData.name}</td>
+                                        <td>{itemData.ppu}</td>
+                                        <td>{itemData.price}</td>
+                                        <td><a href={itemData.href} target="_blank" className='button'>Buy Now</a></td>
+                                    </tr>
+                                    ))}
+                                    </tbody>
+                                </table>
+                                 ) : (
+                                    <h2>No data available</h2>
+                                  )}
+                            </div>
+                        </div>
+                        <div className="col-2">
+                         <div className="banner-ad">
+                            <p>Banner Ad</p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-8">
-                    <div class="search-results">
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Product Name</th>
-                                    <th>Link</th>
-                                    <th>Price</th>
-                                    <th>PPU</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Rice</td>
-                                    <td>www.amazon.com</td>
-                                    <td>$10</td>
-                                    <td>$1.2</td>
-                                    <td><button class="buy">Buy Now</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Rice</td>
-                                    <td>www.amazon.com</td>
-                                    <td>$10</td>
-                                    <td>$1.2</td>
-                                    <td><button class="buy">Buy Now</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Rice</td>
-                                    <td>www.amazon.com</td>
-                                    <td>$10</td>
-                                    <td>$1.2</td>
-                                    <td><button class="buy">Buy Now</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Rice</td>
-                                    <td>www.amazon.com</td>
-                                    <td>$10</td>
-                                    <td>$1.2</td>
-                                    <td><button class="buy">Buy Now</button></td>
-                                </tr>
-                                <tr>
-                                    <td>Rice</td>
-                                    <td>www.amazon.com</td>
-                                    <td>$10</td>
-                                    <td>$1.2</td>
-                                    <td><button class="buy">Buy Now</button></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <div class="col-2">
-                 <div class="banner-ad">
-                    <p>Banner Ad</p>
-                </div>
-            </div>
+            </section>
         </div>
-    </section>
-</div>
-</div>
-      <Footer />
-    </div>
-  )
-}
+        </div>
+              {/* <Footer /> */}
+            </div>
+          )
+    }
 
 export default Search
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,85 +1,172 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 import "./Hero.css"
+import Loading from '../../Pages/Loading/Loading';
+import Search from '../../Pages/Search/Search';
 import { NavLink } from "react-router-dom";
 
 const Hero = () => {
+    const [item, setItem] = useState('');
+    const [unit, setUnit] = useState('');
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchClicked, setSearchClicked] = useState(false);
+
+    // State variables for checkboxes
+    const [amazonChecked, setAmazonChecked] = useState(false);
+    const [walmartChecked, setWalmartChecked] = useState(false);
+    const [costcoChecked, setCostcoChecked] = useState(false);
+    const [dollartreeChecked, setDollartreeChecked] = useState(false);
+
+    const handleInputChange = (event) => {
+        setItem(event.target.value);
+    };
+
+    const handleUnitChange = (event) => {
+        setUnit(event.target.value);
+    };
+
+    const constructAPIUrl = () => {
+    const baseUrl = 'http://74.208.39.34/search';
+    const queryParams = [`item=${item}`, `unit=${unit}`];
+
+    // Add vendor query params based on checkbox values
+    if (amazon) {
+        queryParams.push('amazon=y');
+    }
+
+    if (costco) {
+        queryParams.push('costco=y');
+    }
+
+    if (walmart) {
+        queryParams.push('walmart=y');
+    }
+
+    if (dollartree) {
+        queryParams.push('dollartree=y');
+    }
+
+    // Add other vendors as needed (e.g., walmart, dollartree)
+
+    return `${baseUrl}?${queryParams.join('&')}`;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!(amazonChecked || walmartChecked || costcoChecked || dollartreeChecked)) {
+        alert('Please check at least one checkbox.');
+        return;
+    }
+
+    setSearchClicked(true);
+
+    const apiUrl = constructAPIUrl();
+
+    setIsLoading(true);
+
+    // Use Axios to make the API request and fetch data in JSON format
+        axios
+          .get(apiUrl)
+          .then((response) => {
+            // Handle the fetched data (e.g., update state to display it in the table)
+            setData(response.data);
+            console.log(response.data);
+            console.log(typeof(response.data));
+            //goto('/search');
+          })
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+          })
+          .finally(() => {
+            setIsLoading(false); // Set isLoading to false after data is fetched (or if an error occurs)
+          });
+      };
+
     return(
-<div id="page">
-    <div className="container">
-        <section className="main-section">
-            <div className="flex-row">
-                <div className="col-2">
-                    <div className="banner-ad">
-                        <p>Banner Ad</p>
-                    </div>
-                </div>
-                <div className="col-8">
-                    <div className="main-form">
-                        <h1>BEST PRICE<br/>LARGEST QUANTITY!</h1>
-                        <p>Find the cheapest items for sale online by the pound, ounce, gallon, quart, and more!. From Over Multiple venders </p>
-                        <div className="my-form">
-                            <form action="" method="">
-                                <div className="inputs">
-                                    <input placeholder="Product name" className="prod-input" name="" id="" required="" type="text" />
-                                    <select className="unit-select" name="desired_units" id="units" required="">
-                                        <option selected="" value="">Select Which Units To Analyze...</option>
-                                        <option value="Count">Count</option>
-                                        <optgroup label="Weights &amp; Volumes">
-                                            <option value="Lb">Pounds</option>
-                                            <option value="Oz">Ounces</option>
-                                            <option value="Kg">Kilograms</option>
-                                            <option value="Gram">Grams</option>
-                                            <option value="Mg">Milligrams</option>
-                                            <option value="Fluid_Ounce">Fluid Ounces</option>
-                                            <option value="Gallon">Gallons</option>
-                                            <option value="Liter">Liters</option>
-                                            <option value="Milliliter">Milliliters</option>
-                                            <option value="Cup">Cups</option>
-                                            <option value="Quart">Quarts</option>
-                                            <option value="Pint">Pints</option>
-                                        </optgroup>
-                                        <optgroup label="Computer Measurements">
-                                            <option value="Terabyte">Terabytes (TB)</option>
-                                            <option value="Gigabyte">Gigabytes (GB)</option>
-                                            <option value="Megabyte">Megabytes (MB)</option>
-                                        </optgroup>
-                                    </select>
+        <div id="page">
+            <div className="container">
+                <section className="main-section">
+                    <div className="flex-row">
+                        <div className="col-2">
+                            <div className="banner-ad">
+                                <p>Banner Ad</p>
+                            </div>
+                        </div>
+                        <div className="col-8">
+                            <div className="main-form">
+                                <h1>BEST PRICE<br/>LARGEST QUANTITY!</h1>
+                                <p>Find the cheapest items for sale online by the pound, ounce, gallon, quart, and more!. From Over Multiple venders </p>
+                                <div className="my-form">
+                                    <form onSubmit={handleSubmit}>
+                                        <div className="inputs">
+                                            <input placeholder="Product name" className="prod-input" name="name" id="name" required="" type="text" value={item} onChange={handleInputChange}/>
+                                            <select value={unit} onChange={handleUnitChange} className="unit-select" name="desired_units" id="units" required="">
+                                                <option selected="" value="">Select Unit</option>
+                                                <option value="count">Count</option>
+                                                <optgroup label="Weights &amp; Volumes">
+                                                    <option value="lb">Pounds</option>
+                                                    <option value="oz">Ounces</option>
+                                                    <option value="kg">Kilograms</option>
+                                                    <option value="g">Grams</option>
+                                                    <option value="mg">Milligrams</option>
+                                                    <option value="gal">Gallons</option>
+                                                    <option value="Liter">Liters</option>
+                                                    <option value="l">Milliliters</option>
+                                                    <option value="cups">Cups</option>
+                                                    <option value="qt">Quarts</option>
+                                                    <option value="pint">Pints</option>
+                                                </optgroup>
+                                                <optgroup label="Computer Measurements">
+                                                    <option value="Terabyte">Terabytes (TB)</option>
+                                                    <option value="Gigabyte">Gigabytes (GB)</option>
+                                                    <option value="Megabyte">Megabytes (MB)</option>
+                                                </optgroup>
+                                            </select>
+                                        </div>
+                                        <div className="vendor-ticks">
+                                        <div className="vendor-head"><h2>VENDORS:</h2></div>
+                                            <div className="vendor-tick-box">
+                                                <div className="vendor-tick"><input type="checkbox" name="options" id="amazon" checked={amazonChecked} onChange={(e) => setAmazonChecked(e.target.checked)}/><label >AMAZON</label></div>
+                                                <div className="vendor-tick"><input type="checkbox" name="options" id="walmart" checked={walmartChecked} onChange={(e) => setWalmartChecked(e.target.checked)}/><label >WALMART</label></div>
+                                                <div className="vendor-tick"><input type="checkbox" name="options" id="costco" checked={costcoChecked} onChange={(e) => setCostcoChecked(e.target.checked)}/><label >COSTCO</label></div>
+                                                <div className="vendor-tick"><input type="checkbox" name="options" id="dollartree" checked={dollartreeChecked} onChange={(e) => setDollartreeChecked(e.target.checked)}/><label >DOLLARTREE</label></div>
+                                            </div>
+                                        </div>
+                                        <div className="submit-btn">
+                                            {/* <NavLink to="/search" className="submit-button" type="submit">Check</NavLink> */}
+                                            <button type="submit">Check</button>
+                                        </div>
+                                    </form>
                                 </div>
-                                <div className="vendor-ticks">
-                                <div className="vendor-head"><h2>VENDORS:</h2></div>
-                                    <div className="vendor-tick-box">
-                                        <div className="vendor-tick"><input type="checkbox" name="" /><label>AMAZON</label></div>
-                                        <div className="vendor-tick"><input type="checkbox" name="" /><label>WALMART</label></div>
-                                        <div className="vendor-tick"><input type="checkbox" name="" /><label>COSTCO</label></div>
-                                        <div className="vendor-tick"><input type="checkbox" name="" /><label>DOLLARTREE</label></div>
-                                    </div>
-                                </div>
-                                <div className="submit-btn">
-                                    <NavLink to="/search" className="submit-button" >Check</NavLink>
-                                </div>
-                            </form>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="banner-ad">
+                                <p>Banner Ad</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="col-2">
-                    <div className="banner-ad">
-                        <p>Banner Ad</p>
+                    {searchClicked && isLoading && <Loading />}
+                    {data && data.length > 0 && !isLoading && searchClicked ? (
+                        <Search data={data} />
+                    ) : (
+                        <div></div>
+                    ) }
+                </section>
+                <section className="brands-logo-section">
+                    <div className="brands-logos">
+                        <ul>
+                            <li><NavLink to="https://www.amazon.com" target="_blank"><img src="./amazon.png" /></NavLink></li>
+                            <li><NavLink to="https://www.walmart.com" target="_blank"><img src="./walmart.png" /></NavLink></li>
+                            <li><NavLink to="https://www.costco.com" target="_blank"><img src="./costco.png" /></NavLink></li>
+                            <li><NavLink to="https://www.dollartree.com " target="_blank"><img src="./dollartree.png" /></NavLink></li>
+                        </ul>
                     </div>
-                </div>
+                </section>
             </div>
-        </section>
-        <section className="brands-logo-section">
-            <div className="brands-logos">
-                <ul>
-                    <li><NavLink to="https://www.amazon.com" target="_blank"><img src="./amazon.png" /></NavLink></li>
-                    <li><NavLink to="https://www.walmart.com" target="_blank"><img src="./walmart.png" /></NavLink></li>
-                    <li><NavLink to="https://www.costco.com" target="_blank"><img src="./costco.png" /></NavLink></li>
-                    <li><NavLink to="https://www.dollartree.com " target="_blank"><img src="./dollartree.png" /></NavLink></li>
-                </ul>
-            </div>
-        </section>
-    </div>
-</div>
+        </div>
     )
 }
 
